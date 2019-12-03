a1a38431180feab08b4014db803488abd6a2d701
tmyfxjy <tmyyes@163.com>
2019-12-03 09:44:50 +0800

    add pages

---
+++ web/js/tag.js
@@ -0,0 +1,6 @@
+/** Open Tag Addition */
+function opentag() {
+    window.open ("tag.html", "newwindow", "height=100, width=400, toolbar=no,menubar=no, scrollbars=no, resizable=no, location=no, status=no")
+//写成一行
+}
+//-->
